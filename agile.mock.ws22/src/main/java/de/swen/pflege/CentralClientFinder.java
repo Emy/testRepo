62c76078c458d34b6221e4c547a5a13e336f7b2a
rds <rds@schimkat.org>
2023-01-18 11:11:16 +0100

    initial

---
+++ agile.mock.ws22/src/main/java/de/swen/pflege/CentralClientFinder.java
@@ -0,0 +1,18 @@
+package de.swen.pflege;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Nicht Editieren
+
+/**
+ * Anbindung an die zentrale Klient-Verwaltungkomponente.
+ *
+ */
+public class CentralClientFinder {
+
+	public List<String> getClients(String name) {
+		return new ArrayList<String>();
+	}
+
+}
---
+++ agile.mock.ws22/src/test/java/de/swen/pflege/TestTourplan.java
@@ -0,0 +1,20 @@
+package de.swen.pflege;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class TestTourplan {
+	// TODO 
+	
+
+}
