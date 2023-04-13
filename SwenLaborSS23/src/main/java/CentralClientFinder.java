0b5d8a30aa00f4886cb3659b495126ab0a6c11cc
AnnaSutasinee <103565265+AnnaSutasinee@users.noreply.github.com>
2023-04-13 16:32:02 +0200

    Add files via upload

---
+++ SwenLaborSS23/src/main/java/CentralClientFinder.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Nicht Editieren
+
+/**
+ * Anbindung an die zentrale Klient-Verwaltungkomponente.
+ *
+ */
+public class CentralClientFinder {
+
+    public List<String> getClients(String name) {
+        return new ArrayList<String>();
+    }
+
+}
---
+++ SwenLaborSS23/src/main/java/Tourplan.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ein Tourplan enthält eine Liste von Klienten, die
+ * von der Pflegefachkfraft behandelt werden sollten.
+ * Im Tourplan sollten keine redundanten Klientennamen
+ * enthalten sein. Die Liste der Klienten wird in zwei
+ * Diensten geführt.
+ *
+ */
+public class Tourplan {
+    private CentralClientFinder centralFinder;
+    private SapService service;
+    private String name;
+
+    Tourplan(String name, CentralClientFinder f, SapService s) {
+        this.name = name;
+        centralFinder = f;
+        service = s;
+    }
+
+    /**
+     *
+     * @return List der Klienten (ohne Duplikate)
+     */
+    public List<String>  getClients() {
+
+        List<String> list = new ArrayList<String>();
+        list.addAll(centralFinder.getClients(name));
+        list = addWithoutDuplicates(list, service.getSapClientsForName(name));
+
+        return(list);
+    }
+
+    /**
+     *
+     * @param list erste Liste mit Klienten
+     * @param l zweite Liste mit Klienten
+     * @return bereinigte Liste der Klienten (keine Duplikate enthalten)
+     */
+    private List<String> addWithoutDuplicates(List<String> list, List<String> l) {
+        // TODO noch werden keine Duplikate entfernt ...
+
+        return(l);
+    }
+}
