3c13ccb9649e991804ea90996ea02f9d962b8f49
AnnaSutasinee <103565265+AnnaSutasinee@users.noreply.github.com>
2023-04-14 20:24:37 +0200

    Hab versucht die Duplikate zu entfernen...

--- SwenLaborSS23/src/main/java/Tourplan.java
+++ SwenLaborSS23/src/main/java/Tourplan.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Ein Tourplan enthält eine Liste von Klienten, die
@@ -8,7 +9,6 @@ import java.util.List;
  * Im Tourplan sollten keine redundanten Klientennamen
  * enthalten sein. Die Liste der Klienten wird in zwei
  * Diensten geführt.
- *
  */
 public class Tourplan {
     private CentralClientFinder centralFinder;
@@ -17,39 +17,37 @@ public class Tourplan {
 
     Tourplan(String name, CentralClientFinder f, SapService s) {
         this.name = name;
-        if ( name.length() <= 3) {
+        if (name.length() < 3) {
             throw new IllegalArgumentException("Der Name muss mindestens 3 Zeichen lang sein!");
-        } else if (name.length() >= 32) {
-            throw new IllegalArgumentException("Der Name darf maximal 32 Zeichen lang sein!");
+        } else if (name.length() > 31) {
+            throw new IllegalArgumentException("Der Name darf maximal 31 Zeichen lang sein!");
         } else if (name == null) {
             throw new IllegalArgumentException("Name darf nicht leer sein!");
         }
         centralFinder = f;
         service = s;
     }
-//das ist ein Test
+
     /**
-     *
      * @return List der Klienten (ohne Duplikate)
      */
-    public List<String>  getClients() {
+    public List<String> getClients() {
 
         List<String> list = new ArrayList<String>();
         list.addAll(centralFinder.getClients(name));
         list = addWithoutDuplicates(list, service.getSapClientsForName(name));
 
-        return(list);
+        return (list);
     }
 
     /**
-     *
      * @param list erste Liste mit Klienten
-     * @param l zweite Liste mit Klienten
+     * @param l    zweite Liste mit Klienten
      * @return bereinigte Liste der Klienten (keine Duplikate enthalten)
      */
     private List<String> addWithoutDuplicates(List<String> list, List<String> l) {
         // TODO noch werden keine Duplikate entfernt ...
-
-        return(l);
+        l = list.stream().distinct().collect(Collectors.toList());
+        return (l);
     }
 }
